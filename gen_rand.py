f7e3347b88946e1db8c36d57aa59023b05d90d37
V_Bekasov <vitalijs.bekasovs@gmail.com>
2022-09-03 14:54:38 +0200

    rewriting from sh to py

--- gen_rand.py
+++ gen_rand.py
@@ -1,27 +1,35 @@
 import random
 import sys
+import pandas as pd
+import numpy as np
 
 class Rand_set:
-	def __init__(self, tot_numb):
-		self.__numb = tot_numb
-		self.__row = set()
-		self.__gen_row()
-
-	def __gen_row(self):
-		tmp = 0
-		r_len = 0
-		for i in range(self.__numb):
-			r_len += 1
-			while (r_len > len(self.__row)):
-				tmp = random.randint(-999999, 999999)
-				self.__row.add(tmp)
+	def __init__(self, i_start, i_stop, i_step):
+		self.__sss = np.arange(int(i_start), int(i_stop), int(i_step))
 	
-	def prnt_row(self):
-		for i in self.__row:
-			print(i, end = ' ')
+	def gen_rand_prnt(self, prnt = 0, ret = 0):
+		#out = set()
+		if (ret != 0):
+			r_arr = np.array([], dtype=int)
+		for rng in self.__sss:
+			l = 0
+			out = np.random.randint(low= -999999, high= 999999, size= rng)
+			if (prnt != 0):
+				print(out)
+			if (ret != 0):
+				r_arr = np.append(r_arr, set(out))
+			out = set()
+		if (ret != 0):
+			return r_arr
+
+	def test_fun(self, tst):
+		print("TEST TEXT", tst)
+
+	def __del__(self):
+		print("Obj destroyed")
 
 if __name__ == "__main__":
-	numb = int(sys.argv[1])
-	Obj1 = Rand_set(numb)
-	Obj1.prnt_row()
+	Obj1 = Rand_set(sys.argv[1], sys.argv[2], sys.argv[3])
+	out = Obj1.gen_rand_prnt(ret= 1)
+	print(out)
 	del(Obj1)

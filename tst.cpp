3fff7dc95513e6a765144f361f6a5fec7597e91c
V_Bekasov <vitalijs.bekasovs@gmail.com>
2022-09-20 14:40:13 +0200

    linked list

--- tst.cpp
+++ tst.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-template <class T>
+template <typename T>
 	struct fst
 	{
-		T	m;
+		T		m;
+		fst<T>	*next;
 	};
 
 template <class T>
@@ -13,23 +14,66 @@ template <class T>
 		fst<T>	loc;
 		T		var;
 	public:
-		void prnt_str_type();
+		void	prnt_str_type();
+		fst<T>*	add_el(T nel);
+		fst<T>*	ret_last();
+		fst<T>*	prnt_list();
 	};
 	
 template<class T> void c_scnd<T>::prnt_str_type()
 {
 	std::cout << "fst " << sizeof(var) << std::endl;
+	loc.m = 3;
+	loc.next = NULL;
+	std::cout << "struct mem : " << loc.m << std::endl;
 }
+
+template<class T> fst<T>* c_scnd<T>::prnt_list()
+{
+	fst<T>	*el;
+	el = &loc;
+	std::cout<<"List : ";
+	while (el->next != NULL)
+	{
+		std::cout << el->m << " ";
+		el = el->next;
+	}
+	std::cout<< el->m <<std::endl;
+	return el;
+}
+
+template<class T> fst<T>* c_scnd<T>::ret_last()
+{
+	fst<T>	*el;
+	el = &loc;
+	while (el->next != NULL)
+		el = el->next;
+
+	return el;
+}
+
+template<class T> fst<T>* c_scnd<T>::add_el(T nel)
+{
+	fst<T>	*el;
+	el = new(fst<T>);
+	el->m = nel;
+	std::cout << "New el " << el->m << std::endl;
+	
+	fst<T>	*last = ret_last();
+	last->next = el;
+	return el;
+} 
 
 int main()
 {
 	fst<int>	a;
 	fst<char>	b;
 
-	std::cout << "ta " << sizeof(a.m) << " tb " << sizeof(b.m) << std::endl;
-
 	c_scnd<int> Obj1;
 	Obj1.prnt_str_type();
+	Obj1.add_el(1234);
+	Obj1.add_el(987);
+	Obj1.prnt_list();
 	
 	return 0;
 }

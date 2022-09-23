607815d6d0b2e0c32a86ec12dcfa46f573f11fdf
V_Bekasov <vitalijs.bekasovs@gmail.com>
2022-09-23 20:49:07 +0200

    ll obj in char arr

--- tl.h
+++ tl.h
@@ -1,41 +1,72 @@
 #ifndef TL_H
 #define TL_H
 
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include "stdio.h"
+
 namespace ll
-{   
-    //template<class T> 
-        class UniList
+{       
+
+    enum    etype
+    { eint, efloat, echar, echar_ar, eobj};
+
+    struct  node
     {
-    private:
-        template<auto val>
-            struct ls
-        {
-            static constexpr auto var = val;
-            ls*  next;
-        };
-
-        //ls*  root;
-
-    public:
-        UniList(const auto fst);
-        ~UniList();
+        char*   obj_cont;
+        etype   et;
+        std::string s_type;
+        node*   next;
+        //void    set_type(std::string etype);
+        void    ret_type();
     };
-    
-    /*template<class T> */ UniList::UniList(const auto fst)
+
+    void node::ret_type()
     {
-        const char tmp = 'A';
-        const auto tmp1 = fst;
-        ls<tmp1>* root = nullptr;
-        //root = new ls;
-        //root->var = fst;
-        std::cout<<fst<<std::endl;
-        root->next = nullptr;
+        et = eobj;
+        std::cout << et << std::endl;
     }
-    
-    /*
-    template<class T> UniList<T>::~UniList()
+
+    //-----------------------
+
+    class   c_dummy
     {
-    }*/
+        public:
+            int val;
+            ~c_dummy();
+    };
+
+    c_dummy::~c_dummy()
+    {
+        std::cout << "Dist\n";
+    }
+
+    class   c_row
+    {
+        private:
+            node    root;
+        public:
+            void write_class();
+            void print_val();
+    };
+
+    //------------------------
+
+    void c_row::write_class()
+    {
+        root.obj_cont = new char[sizeof(c_dummy)];
+        c_dummy* tmpObj = new (root.obj_cont) c_dummy;
+        tmpObj->val = 1234;
+        root.s_type = typeid(tmpObj).name();
+    }
+
+    void c_row::print_val()
+    {
+        c_dummy* nObj = (c_dummy*)root.obj_cont;
+        std::cout << nObj->val << std::endl << root.s_type << std::endl;
+        //root.ret_type();
+    }
     
 }
 

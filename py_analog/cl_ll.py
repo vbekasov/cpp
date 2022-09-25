b48180bfa984bdeab0661aa93725fffd7a657731
V_Bekasov <vitalijs.bekasovs@gmail.com>
2022-09-25 10:04:01 +0200

    ll py try

---
+++ py_analog/cl_ll.py
@@ -0,0 +1,19 @@
+class DT:
+    def __init__(self, input):
+        self.__data = input
+        self.__next = None
+
+class LL:
+    def __init__(self, input):
+        self.__first = DT(input)
+
+    def find_lst(self):
+        
+    
+    def add_val(self, input):
+        TmpObj = DT(input)
+        
+
+if __name__ == "__main__":
+    print("Main Works")
+    Obj1 = LL(123)

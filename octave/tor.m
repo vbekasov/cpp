59420adf6cbd5bb8c29712bca0a766cc4bedbf9a
Vitalijs Bekasov <vitalijs.bekasovs@gmail.com>
2023-02-26 16:10:21 -0500

    tor prog

--- octave/tor.m
+++ octave/tor.m
@@ -1,17 +1,19 @@
-function m = tor(r1, r2)
+function m = tor(r1 = 3, r2 = 2)
   l = [-pi:0.4:pi+0.2];
   l = repmat(l, 1, 5);
   l = reshape(l, [], 5);
-  x = ((ones(17, 5) .* cos(l)*r1) .* [1 2 3 2 1])';
-  y = ((ones(17, 5) .* sin(l)*r1) .* [1 2 3 2 1])';
-  z = (zeros(17, 5) + [0 1 0 -1 0])';
+  x = ((ones(17, 5) .*cos(l)*r1) .* [1 2 3 2 1])';
+  y = ((ones(17, 5) .*sin(l)*r1) .* [1 2 3 2 1])';
+  z = (zeros(17, 5)+(cos([-pi/2:pi/2:pi+pi/2])*r1+r1))';
+  
   #plot3(x, y, z, '-.')
-  #surf(x, y, z, 'FaceAlpha', 0.2)
   surf(x, y, z, 'FaceAlpha', 0.2, 'FaceColor', 'interp')
+  hold on
+  plot3(0, 0, r1*6)
   pbaspect([1 1 1])
   xlabel('cos(x)');
   ylabel('sin(y)');
   zlabel('ring z');
   grid on
+  hold off
 end
-

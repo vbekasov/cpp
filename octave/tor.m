a03dee949adf13056bffd6b87bddc03854cf6d1f
Vitalijs Bekasov <vitalijs.bekasovs@gmail.com>
2023-02-26 20:28:28 -0500

    tor prog1

--- octave/tor.m
+++ octave/tor.m
@@ -1,12 +1,21 @@
-function m = tor(r1 = 3, r2 = 2)
-  l = [-pi:0.4:pi+0.2];
-  l = repmat(l, 1, 5);
-  l = reshape(l, [], 5);
-  x = ((ones(17, 5) .*cos(l)*r1) .* [1 2 3 2 1])';
-  y = ((ones(17, 5) .*sin(l)*r1) .* [1 2 3 2 1])';
-  z = (zeros(17, 5)+(cos([-pi/2:pi/2:pi+pi/2])*r1+r1))';
+function m = tor(r1 = 10)
+  # number of segments on XY surface = seg * 2 
+  # horizontal points are defined by l
+  seg = 10;
+  #l = [-pi:pi/seg:pi+pi/seg];
+  l = [-pi:pi/seg:pi];
+  # number of segments on vertical section = seg * 2
+  # for clear represintation seg should be one of 2 power
+  # vertical points are defined by lk 
+  seg = 8;
+   l = repmat(l, 1, seg*2+1);
+  l = reshape(l, [], seg*2+1);
+  lk = sin([-pi/2:pi/seg:pi+pi/2])+2;
   
-  #plot3(x, y, z, '-.')
+  x = ((ones(size(l)(1), size(lk)(1)).*cos(l)*r1).*lk)';
+  y = ((ones(size(l)(1), size(lk)(1)).*sin(l)*r1).*lk)';
+  z = (zeros(size(l)(1), size(lk)(1))+(cos([-pi/2:pi/seg:pi+pi/2])*r1+r1))';
+  
   surf(x, y, z, 'FaceAlpha', 0.2, 'FaceColor', 'interp')
   hold on
   plot3(0, 0, r1*6)
